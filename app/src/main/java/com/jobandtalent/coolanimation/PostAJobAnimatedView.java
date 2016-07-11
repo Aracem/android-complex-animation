6afcff8324be6e6cb827af9349bab3f1e9789801
Aracem <marcostruji@gmail.com>
2016-07-11 14:49:11 +0200

    Final adjustements to the circle animation

--- app/src/main/java/com/jobandtalent/coolanimation/PostAJobAnimatedView.java
+++ app/src/main/java/com/jobandtalent/coolanimation/PostAJobAnimatedView.java
@@ -11,14 +11,13 @@ import android.content.Context;
 import android.os.Build;
 import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.BounceInterpolator;
 import android.widget.FrameLayout;
 
-import java.util.HashMap;
 import java.util.Random;
 
 import butterknife.ButterKnife;
@@ -29,27 +28,36 @@ public class PostAJobAnimatedView extends FrameLayout {
     private static final int CIRCLE_ANIMATION_TIME = 5000;
     private static final int CIRCLE_ANIMATION_DELAY_ONE = 1666;
     private static final int CIRCLE_ANIMATION_DELAY_TWO = 3333;
+    private static final int AVATAR_ANIMATION_MAX_TIME = 2500;
+    private static final int AVATAR_ANIMATION_MIN_TIME = 2000;
+    private static final int AVATAR_FIRST_DELAY = 1500;
+    private static final int AVATAR_SECOND_DELAY = 8666;
 
-    @InjectView(R.id.v_circle_1) View mCircleOne;
-    @InjectView(R.id.v_circle_2) View mCircleTwo;
-    @InjectView(R.id.v_circle_3) View mCircleThree;
-    @InjectView(R.id.v_avatar_1) PostAJobAnimationCircleImageView mAvatarOne;
-    @InjectView(R.id.v_avatar_2) PostAJobAnimationCircleImageView mAvatarTwo;
+    @InjectView(R.id.v_circle_1) View circleOne;
+    @InjectView(R.id.v_circle_2) View circleTwo;
+    @InjectView(R.id.v_circle_3) View circleThree;
+    @InjectView(R.id.v_avatar_1) PostAJobAnimationCircleImageView avatarOne;
+    @InjectView(R.id.v_avatar_2) PostAJobAnimationCircleImageView avatarTwo;
 
-    private static HashMap<Integer, Integer> mAvatarResources;
+    private static SparseIntArray avatarResources;
+    private boolean isViewReady = false;
+    private boolean isWaitingToStart = false;
+    private int minAvatarMargin = 0;
+    private int avatarSize = 0;
+    private int avatarResourcesIndex = 0;
 
     static {
-        mAvatarResources = new HashMap<>();
+        avatarResources = new SparseIntArray();
 
-        mAvatarResources.put(0, R.drawable.avatar_1);
-        mAvatarResources.put(1, R.drawable.avatar_2);
-        mAvatarResources.put(2, R.drawable.avatar_3);
-        mAvatarResources.put(3, R.drawable.avatar_4);
-        mAvatarResources.put(4, R.drawable.avatar_5);
-        mAvatarResources.put(5, R.drawable.avatar_6);
-        mAvatarResources.put(6, R.drawable.avatar_7);
-        mAvatarResources.put(7, R.drawable.avatar_8);
-        mAvatarResources.put(8, R.drawable.avatar_9);
+        avatarResources.put(0, R.drawable.avatar_1);
+        avatarResources.put(1, R.drawable.avatar_2);
+        avatarResources.put(2, R.drawable.avatar_3);
+        avatarResources.put(3, R.drawable.avatar_4);
+        avatarResources.put(4, R.drawable.avatar_5);
+        avatarResources.put(5, R.drawable.avatar_6);
+        avatarResources.put(6, R.drawable.avatar_7);
+        avatarResources.put(7, R.drawable.avatar_8);
+        avatarResources.put(8, R.drawable.avatar_9);
     }
 
     public PostAJobAnimatedView(Context context) {
@@ -69,44 +77,56 @@ public class PostAJobAnimatedView extends FrameLayout {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
-
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-
         LayoutInflater.from(getContext()).inflate(R.layout.view_postajob_animation, this);
         ButterKnife.inject(this);
         if (isInEditMode()) {
             renderEditMode();
-            return;
         }
     }
 
     public void startAnimation() {
-        launchCirclesAnimation();
-        launchAvatarsAnimation();
+        if (isViewReady) {
+            isWaitingToStart = false;
+            calculateDefaultSizes();
+            launchCirclesAnimation();
+            launchAvatarsAnimation();
+        } else {
+            isWaitingToStart = true;
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        isViewReady = true;
+        if (isWaitingToStart) {
+            startAnimation();
+        }
     }
 
     private void launchCirclesAnimation() {
-        mCircleTwo.setScaleX(0f);
-        mCircleTwo.setScaleY(0f);
-        mCircleThree.setScaleX(0f);
-        mCircleThree.setScaleY(0f);
+        circleTwo.setScaleX(0f);
+        circleTwo.setScaleY(0f);
+        circleThree.setScaleX(0f);
+        circleThree.setScaleY(0f);
 
-        ObjectAnimator animator = getCircleAnimation(mCircleOne, CIRCLE_ANIMATION_TIME);
+        ObjectAnimator animator = getCircleAnimation(circleOne, CIRCLE_ANIMATION_TIME);
         animator.setRepeatMode(ValueAnimator.RESTART);
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new FastOutLinearInInterpolator());
         animator.start();
 
-        ObjectAnimator animator2 = getCircleAnimation(mCircleTwo, CIRCLE_ANIMATION_TIME);
+        ObjectAnimator animator2 = getCircleAnimation(circleTwo, CIRCLE_ANIMATION_TIME);
         animator2.setStartDelay(CIRCLE_ANIMATION_DELAY_ONE);
         animator2.setRepeatMode(ValueAnimator.RESTART);
         animator2.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new BounceInterpolator());
         animator2.start();
 
-        ObjectAnimator animator3 = getCircleAnimation(mCircleThree, CIRCLE_ANIMATION_TIME);
+        ObjectAnimator animator3 = getCircleAnimation(circleThree, CIRCLE_ANIMATION_TIME);
         animator3.setStartDelay(CIRCLE_ANIMATION_DELAY_TWO);
         animator3.setRepeatMode(ValueAnimator.RESTART);
         animator3.setRepeatCount(ValueAnimator.INFINITE);
@@ -115,41 +135,30 @@ public class PostAJobAnimatedView extends FrameLayout {
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    public static ObjectAnimator getCircleAnimation(View view, int millis) {
+    private ObjectAnimator getCircleAnimation(View view, int millis) {
         PropertyValuesHolder pvhScaleX = PropertyValuesHolder.ofKeyframe(View.SCALE_X,
                 Keyframe.ofFloat(0f, 0.01f),
                 Keyframe.ofFloat(.2f, .65f),
-                Keyframe.ofFloat(.3f, .83f),
-                Keyframe.ofFloat(.35f, .86f),
-                Keyframe.ofFloat(.4f, .885f),
-                Keyframe.ofFloat(.45f, .905f),
-                Keyframe.ofFloat(.5f, .915f),
+                Keyframe.ofFloat(.3f, .84f),
+                Keyframe.ofFloat(.35f, .87f),
+                Keyframe.ofFloat(.4f, .895f),
+                Keyframe.ofFloat(.45f, .915f),
+                Keyframe.ofFloat(.5f, .930f),
                 Keyframe.ofFloat(.55f, .940f),
-                Keyframe.ofFloat(.6f, .955f),
+                Keyframe.ofFloat(.6f, .945f),
                 Keyframe.ofFloat(1f, 1f)
         );
-
-//        Keyframe.ofFloat(0f, 0.01f),
-//                Keyframe.ofFloat(.3f, .7f),
-//                Keyframe.ofFloat(.4f, .79f),
-//                Keyframe.ofFloat(.45f, .835f),
-//                Keyframe.ofFloat(.5f, .865f),
-//                Keyframe.ofFloat(.55f, .895f),
-//                Keyframe.ofFloat(.6f, .92f),
-//                Keyframe.ofFloat(.65f, .935f),
-//                Keyframe.ofFloat(.7f, .95f),
-//                Keyframe.ofFloat(1f, 1f)
 
         PropertyValuesHolder pvhScaleY = PropertyValuesHolder.ofKeyframe(View.SCALE_Y,
                 Keyframe.ofFloat(0f, 0.01f),
                 Keyframe.ofFloat(.2f, .65f),
-                Keyframe.ofFloat(.3f, .83f),
-                Keyframe.ofFloat(.35f, .86f),
-                Keyframe.ofFloat(.4f, .885f),
-                Keyframe.ofFloat(.45f, .905f),
-                Keyframe.ofFloat(.5f, .915f),
+                Keyframe.ofFloat(.3f, .84f),
+                Keyframe.ofFloat(.35f, .87f),
+                Keyframe.ofFloat(.4f, .895f),
+                Keyframe.ofFloat(.45f, .915f),
+                Keyframe.ofFloat(.5f, .930f),
                 Keyframe.ofFloat(.55f, .940f),
-                Keyframe.ofFloat(.6f, .955f),
+                Keyframe.ofFloat(.6f, .945f),
                 Keyframe.ofFloat(1f, 1f)
         );
 
@@ -166,77 +175,77 @@ public class PostAJobAnimatedView extends FrameLayout {
 
 
     private void launchAvatarsAnimation() {
-        mAvatarOne.setScaleX(0f);
-        mAvatarOne.setScaleY(0f);
-        mAvatarTwo.setScaleX(0f);
-        mAvatarTwo.setScaleY(0f);
+        avatarOne.setScaleX(0f);
+        avatarOne.setScaleY(0f);
+        avatarTwo.setScaleX(0f);
+        avatarTwo.setScaleY(0f);
 
-        mAvatarOne.setImageResource(getNextAvatar());
-        randomizeAvatarPosition(mAvatarOne);
-        ObjectAnimator avatarAnimation = getAvatarAnimation(mAvatarOne, getAvatarRandomTime());
+        avatarOne.setImageResource(getNextAvatar());
+        randomizeAvatarPosition(avatarOne);
+        ObjectAnimator avatarAnimation = getAvatarAnimation(avatarOne, getAvatarRandomTime());
         avatarAnimation.setRepeatMode(ValueAnimator.RESTART);
         avatarAnimation.setRepeatCount(ValueAnimator.INFINITE);
-        avatarAnimation.setStartDelay(1500);
+        avatarAnimation.setStartDelay(AVATAR_FIRST_DELAY);
         avatarAnimation.addListener(new AnimatorListenerAdapter() {
 
             @Override
             public void onAnimationRepeat(Animator animation) {
                 super.onAnimationRepeat(animation);
-                randomizeAvatarPosition(mAvatarOne);
-                mAvatarOne.setImageResource(getNextAvatar());
+                randomizeAvatarPosition(avatarOne);
+                avatarOne.setImageResource(getNextAvatar());
             }
         });
         avatarAnimation.start();
 
-        mAvatarTwo.setImageResource(getNextAvatar());
-        randomizeAvatarPosition(mAvatarTwo);
-        ObjectAnimator avatarAnimation2 = getAvatarAnimation(mAvatarTwo, getAvatarRandomTime());
+        avatarTwo.setImageResource(getNextAvatar());
+        randomizeAvatarPosition(avatarTwo);
+        ObjectAnimator avatarAnimation2 = getAvatarAnimation(avatarTwo, getAvatarRandomTime());
         avatarAnimation2.setRepeatMode(ValueAnimator.RESTART);
         avatarAnimation2.setRepeatCount(ValueAnimator.INFINITE);
-        avatarAnimation2.setStartDelay(8666);
+        avatarAnimation2.setStartDelay(AVATAR_SECOND_DELAY);
         avatarAnimation2.addListener(new AnimatorListenerAdapter() {
 
             @Override
             public void onAnimationRepeat(Animator animation) {
                 super.onAnimationRepeat(animation);
-                randomizeAvatarPosition(mAvatarTwo);
-                mAvatarTwo.setImageResource(getNextAvatar());
+                randomizeAvatarPosition(avatarTwo);
+                avatarTwo.setImageResource(getNextAvatar());
             }
         });
         avatarAnimation2.start();
     }
 
     private int getAvatarRandomTime() {
-        return new Random().nextInt(2500 - 2000) + 2000;
+        return new Random().nextInt(AVATAR_ANIMATION_MAX_TIME - AVATAR_ANIMATION_MIN_TIME) + AVATAR_ANIMATION_MIN_TIME;
     }
 
-    private int mAvatarResourcesIndex = 0;
-
     synchronized private int getNextAvatar() {
-        int result = mAvatarResources.get(mAvatarResourcesIndex);
-        mAvatarResourcesIndex = ++mAvatarResourcesIndex % mAvatarResources.size();
+        int result = avatarResources.get(avatarResourcesIndex);
+        avatarResourcesIndex = ++avatarResourcesIndex % avatarResources.size();
         return result;
     }
 
     private void randomizeAvatarPosition(View view) {
         FrameLayout.LayoutParams params = (LayoutParams) view.getLayoutParams();
-        int width = 720; //getWidth();
-        int height = 880; //getHeight();
-        int minMargin = getContext().getResources().getDimensionPixelOffset(
-                R.dimen.postajob_animation_avatar_min_layout_margin);
-
-
-        int newWidth = new Random().nextInt(width - minMargin) + minMargin + getLeft();
-        int newHeight = new Random().nextInt(height - minMargin) + minMargin + getTop();
-        Log.d("Animation", "Random width: " + newWidth + " : randomHeight: " + newHeight);
+        int width = getWidth();
+        int height = getHeight();
+        int newWidth = new Random().nextInt(width - minAvatarMargin - avatarSize) + minAvatarMargin;
+        int newHeight = new Random().nextInt(height - minAvatarMargin - avatarSize) + minAvatarMargin;
 
         params.topMargin = newHeight;
         params.leftMargin = newWidth;
         view.setLayoutParams(params);
     }
 
+    private void calculateDefaultSizes() {
+        minAvatarMargin = getContext().getResources().getDimensionPixelOffset(
+                R.dimen.postajob_animation_avatar_min_layout_margin);
+        avatarSize = getContext().getResources().getDimensionPixelSize(
+                R.dimen.postajob_animation_avatar);
+    }
+
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    public static ObjectAnimator getAvatarAnimation(View view, int millis) {
+    private ObjectAnimator getAvatarAnimation(View view, int millis) {
         PropertyValuesHolder pvhScaleX = PropertyValuesHolder.ofKeyframe(View.SCALE_X,
                 Keyframe.ofFloat(0f, 0.01f),
                 Keyframe.ofFloat(.035f, 0.45f),
@@ -255,34 +264,13 @@ public class PostAJobAnimatedView extends FrameLayout {
                 Keyframe.ofFloat(1f, 0f)
         );
 
-        PropertyValuesHolder pvhCustom = PropertyValuesHolder.ofKeyframe("mode",
-                Keyframe.ofInt(0f, 0),
-                Keyframe.ofInt(.07f, 1),
-                Keyframe.ofInt(.93f, 1),
-                Keyframe.ofInt(1f, 0)
-        );
-
-        PropertyValuesHolder pvhTranslateX = PropertyValuesHolder.ofKeyframe(View.TRANSLATION_X,
-                Keyframe.ofFloat(0f, 0f),
-                Keyframe.ofFloat(.1f, 0f),
-                Keyframe.ofFloat(.8f, .05f),
-                Keyframe.ofFloat(1f, .05f)
-        );
-
-        PropertyValuesHolder pvhTranslateY = PropertyValuesHolder.ofKeyframe(View.TRANSLATION_Y,
-                Keyframe.ofFloat(0f, 0f),
-                Keyframe.ofFloat(.1f, 0f),
-                Keyframe.ofFloat(.8f, .05f),
-                Keyframe.ofFloat(1f, .05f)
-        );
-
         return ObjectAnimator.ofPropertyValuesHolder(view, pvhScaleX, pvhScaleY).
                 setDuration(millis);
     }
 
     private void renderEditMode() {
-        mCircleOne.setVisibility(VISIBLE);
-        mCircleOne.setScaleX(3f);
-        mCircleOne.setScaleY(3f);
+        circleOne.setVisibility(VISIBLE);
+        circleOne.setScaleX(3f);
+        circleOne.setScaleY(3f);
     }
 }
